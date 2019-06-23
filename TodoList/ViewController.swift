b54020a0345dde11f4809f7cff64669a773c7677
Idris <idrisnovandi@gmail.com>
2019-06-23 19:19:36 +0700

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Idris on 23/06/19.
+//  Copyright © 2019 IdrisLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
